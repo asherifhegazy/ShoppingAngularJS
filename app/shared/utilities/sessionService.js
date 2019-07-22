78e4cf08b8bb94eb5f538b9d064be4a97464f9a7
Ahmed.Sherif <ahmed.sherif@integrant.com>
2019-07-22 18:20:51 +0200

    add  service and session service

---
+++ app/shared/utilities/sessionService.js
@@ -0,0 +1,15 @@
+eShopApp.factory('session', function ($localStorage) {
+    return {
+        getLoggedInUser: function () {
+            return $localStorage.getObject('user');
+        },
+
+        logout: function () {
+            $localStorage.remove('user');
+        },
+
+        login: function (user) {
+            $localStorage.setObject('user', user);
+        }
+    }
+});
